3e06623c8bb4fadc0aad65d0c099a289f0d6d802
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-25 22:35:29 +0300

    Created navbar component, styles etc.

--- Events-Schedule/src/app/events/events-list.component.ts
+++ Events-Schedule/src/app/events/events-list.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector : 'events-list',
-    templateUrl : './events-list.component.html'
+    templateUrl : './events-list.component.html',
+    styleUrls : ['./events-list.component.css']
 })
 
 export class EventsListComponent{
@@ -16,4 +17,5 @@ export class EventsListComponent{
             country : "Greece"
         } 
     }
+
 }
