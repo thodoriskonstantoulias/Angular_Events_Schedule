6a772a9c3472fe3061b52eb7dc65b047a12c088d
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-26 17:44:31 +0300

    Added service

--- Events-Schedule/src/app/app.module.ts
+++ Events-Schedule/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavbarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { NavbarComponent } from './nav/navbar.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [EventService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
