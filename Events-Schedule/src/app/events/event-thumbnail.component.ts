def350c28b6cbbf0f833f1f59ecb8188be632d38
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-26 13:03:55 +0300

    Adding styles and hidden attributes

--- Events-Schedule/src/app/events/event-thumbnail.component.ts
+++ Events-Schedule/src/app/events/event-thumbnail.component.ts
@@ -2,7 +2,8 @@ import {Component , Input} from '@angular/core';
 
 @Component({
     selector : 'event-thumbnail',
-    templateUrl : './event-thumbnail.component.html'
+    templateUrl : './event-thumbnail.component.html',
+    styles : ['.thumbnail {min-height : 210px;}']
 })
 
 export class EventThumbnailComponent{
